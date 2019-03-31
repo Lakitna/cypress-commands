ed5de299cc52c456280f10df3e4412049f0fb665
Sander van Beek <lakitna@gmail.com>
2019-03-31 20:48:58 +0200

    Rebuild text command, added tests

--- app/index.js
+++ app/index.js
@@ -3,4 +3,10 @@ setInterval(() => {
     if (count < 5) {
         $('#list').append('<li>li ' + count + '</li>');
     }
+
+    const elem = $('.counter');
+    const val = +elem.text();
+    if (val < 5) {
+        elem.text(val + 1);
+    }
 }, 100);

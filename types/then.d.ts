c0bc823885584649e86044e5a7236261b47fc996
Sander van Beek <lakitna@gmail.com>
2019-04-10 21:52:23 +0200

    Made Subject explicit any, added reverse order of fn and options

--- types/then.d.ts
+++ types/then.d.ts
@@ -1,5 +1,5 @@
 declare namespace Cypress {
-    interface Chainable<Subject> {
+    interface Chainable<Subject = any> {
         /**
          * Enables you to work with the subject yielded from the previous command.
          *
@@ -39,5 +39,45 @@ declare namespace Cypress {
             log?: boolean;
             retry?: boolean;
         }, fn: (this: ObjectLike, currentSubject: Subject) => void): Chainable<Subject>
+
+        /**
+         * Enables you to work with the subject yielded from the previous command.
+         *
+         * @see https://github.com/Lakitna/cypress-commands/blob/master/docs/then.md
+         */
+        then<S>(fn: (this: ObjectLike, currentSubject: Subject) => Chainable<S>, options: {
+            log?: boolean;
+            retry?: boolean;
+        }): Chainable<S>
+        /**
+         * Enables you to work with the subject yielded from the previous command / promise.
+         *
+         * @see https://github.com/Lakitna/cypress-commands/blob/master/docs/then.md
+         */
+        then<S>(fn: (this: ObjectLike, currentSubject: Subject) => PromiseLike<S>, options: {
+            log?: boolean;
+            retry?: boolean;
+        }): Chainable<S>
+        /**
+         * Enables you to work with the subject yielded from the previous command / promise.
+         *
+         * @see https://github.com/Lakitna/cypress-commands/blob/master/docs/then.md
+         */
+        then<S extends object | any[] | string | number | boolean>(fn: (this: ObjectLike, currentSubject: Subject) => S, options: {
+            log?: boolean;
+            retry?: boolean;
+        }): Chainable<S>
+        /**
+         * Enables you to work with the subject yielded from the previous command.
+         *
+         * @see https://github.com/Lakitna/cypress-commands/blob/master/docs/then.md
+         * @example
+         *    cy.get('.nav').then(($nav) => {})  // Yields .nav as first arg
+         *    cy.location().then((loc) => {})   // Yields location object as first arg
+         */
+        then(fn: (this: ObjectLike, currentSubject: Subject) => void, options: {
+            log?: boolean;
+            retry?: boolean;
+        }): Chainable<Subject>
     }
 }

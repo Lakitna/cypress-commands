62958ddd95adf8857d03bc7cb7e807dcc3d5f0d9
Sander van Beek <lakitna@gmail.com>
2019-04-10 20:54:37 +0200

    Added type definitions for then, changed some documentation

---
+++ types/then.d.ts
@@ -0,0 +1,43 @@
+declare namespace Cypress {
+    interface Chainable<Subject> {
+        /**
+         * Enables you to work with the subject yielded from the previous command.
+         *
+         * @see https://github.com/Lakitna/cypress-commands/blob/master/docs/then.md
+         */
+        then<S>(options: {
+            log?: boolean;
+            retry?: boolean;
+        }, fn: (this: ObjectLike, currentSubject: Subject) => Chainable<S>): Chainable<S>
+        /**
+         * Enables you to work with the subject yielded from the previous command / promise.
+         *
+         * @see https://github.com/Lakitna/cypress-commands/blob/master/docs/then.md
+         */
+        then<S>(options: {
+            log?: boolean;
+            retry?: boolean;
+        }, fn: (this: ObjectLike, currentSubject: Subject) => PromiseLike<S>): Chainable<S>
+        /**
+         * Enables you to work with the subject yielded from the previous command / promise.
+         *
+         * @see https://github.com/Lakitna/cypress-commands/blob/master/docs/then.md
+         */
+        then<S extends object | any[] | string | number | boolean>(options: {
+            log?: boolean;
+            retry?: boolean;
+        }, fn: (this: ObjectLike, currentSubject: Subject) => S): Chainable<S>
+        /**
+         * Enables you to work with the subject yielded from the previous command.
+         *
+         * @see https://github.com/Lakitna/cypress-commands/blob/master/docs/then.md
+         * @example
+         *    cy.get('.nav').then(($nav) => {})  // Yields .nav as first arg
+         *    cy.location().then((loc) => {})   // Yields location object as first arg
+         */
+        then(options: {
+            log?: boolean;
+            retry?: boolean;
+        }, fn: (this: ObjectLike, currentSubject: Subject) => void): Chainable<Subject>
+    }
+}
